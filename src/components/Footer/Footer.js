1936ae5ef2e2afef931e4d29bdeb9b7d419f8e60
Pranta07 <palpranta@gmail.com>
2021-10-18 02:32:40 +0600

    footer-done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,80 @@
+import {
+    faFacebook,
+    faInstagram,
+    faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import brandLogo from "../../images/brand-logo.png";
+import "./Footer.css";
 
 const Footer = () => {
     return (
-        <div>
-            <p>this is footer</p>
+        <div className="footer">
+            <div className="container text-center text-white">
+                <div className="pt-5 mx-auto row row-cols-1 row-cols-md-4 row-cols-lg-4">
+                    <div className="col">
+                        <img src={brandLogo} alt="" width="30" height="30" />
+                        <span> Medicure</span>
+                        <p className="mt-2">
+                            <small>
+                                Medicure provides progressive, and affordable
+                                healthcare, accessible on mobile and online for
+                                everyone.
+                            </small>
+                        </p>
+                    </div>
+                    <div className="col">
+                        <span>Company</span>
+                        <ul className="mt-2 list">
+                            <li>About</li>
+                            <li>Reports</li>
+                            <li>Top doctors</li>
+                            <li>Donate Blood</li>
+                            <li>Covid Portal</li>
+                            <li>Apps</li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <span>District</span>
+                        <ul className="mt-2 list">
+                            <li>Chittagong</li>
+                            <li>Dhaka</li>
+                            <li>Khulna</li>
+                            <li>Rajshahi</li>
+                            <li>Comilla</li>
+                        </ul>
+                    </div>
+                    <div className="col">
+                        <span>Help</span>
+                        <ul className="mt-2 list">
+                            <li>Help Center</li>
+                            <li>Contact support</li>
+                            <li>Instruction</li>
+                            <li>Emergency</li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <h5>Follow us on</h5>
+                    <div>
+                        <span className="px-2">
+                            <FontAwesomeIcon icon={faFacebook} />
+                        </span>
+                        <span className="px-2">
+                            <FontAwesomeIcon icon={faInstagram} />
+                        </span>
+                        <span className="px-2">
+                            <FontAwesomeIcon icon={faTwitter} />
+                        </span>
+                    </div>
+                    <p className="mb-0 text-center">
+                        <small>
+                            All rights reserved ©Medicure,2021|Pranta Pal
+                        </small>
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,38 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import useAuth from "../../hook/useAuth";
+import brandLogo from "../../images/brand-logo.png";
 
 const Header = () => {
+    const { user } = useAuth();
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="/home">Medicare</Navbar.Brand>
+                <Navbar.Brand href="/home">
+                    <img
+                        alt=""
+                        src={brandLogo}
+                        width="30"
+                        height="30"
+                        className="align-top d-inline-block"
+                    />{" "}
+                    Medicure
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Login</Nav.Link>
-                        <Nav.Link href="#pricing">Register</Nav.Link>
+                        <Nav.Link as={Link} to="/home">
+                            Home
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/login">
+                            Login
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/register">
+                            Register
+                        </Nav.Link>
                     </Nav>
-                    <Navbar.Text>Signed in as: Mark Otto</Navbar.Text>
+                    <Navbar.Text>Signed in as: {user.displayName}</Navbar.Text>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
