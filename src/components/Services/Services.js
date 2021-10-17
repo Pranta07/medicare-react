b25f9743f180d63c3d3c3da4c77e57c8c2ad82b8
Pranta07 <palpranta@gmail.com>
2021-10-18 04:16:21 +0600

    fake service data created

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Services = () => {
     return (
-        <div>
+        <div className="text-center">
+            <h1 className="fw-bold">Our Services</h1>
+            <hr className="mx-auto w-25" />
             <p>All Services should be placed here</p>
         </div>
     );
