182421d89cba4ffefcf257a43ae54d8970e1a1c0
Pranta07 <palpranta@gmail.com>
2021-10-18 04:33:16 +0600

    fake services added

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Button, Card, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const Service = () => {
+const Service = ({ service }) => {
     return (
-        <div>
-            <h1>This is single service</h1>
-        </div>
+        <Col>
+            <Card>
+                <Card.Img variant="top" src={service.img} />
+                <Card.Body>
+                    <Card.Title>{service.name}</Card.Title>
+                    <Card.Text>{service.description}</Card.Text>
+                </Card.Body>
+                <Link to={`/details/${service.id}`}>
+                    <Button variant="dark">Details</Button>
+                </Link>
+            </Card>
+        </Col>
     );
 };
 
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, Row } from "react-bootstrap";
+import Service from "../Service/Service";
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch("/fakeData.json")
+            .then((res) => res.json())
+            .then((data) => setServices(data));
+    }, []);
+
     return (
         <div className="text-center">
             <h1 className="fw-bold">Our Services</h1>
             <hr className="mx-auto w-25" />
-            <p>All Services should be placed here</p>
+            <Container>
+                <Row
+                    xs={1}
+                    md={2}
+                    lg={4}
+                    className="g-4 d-flex justify-content-center"
+                >
+                    {services.map((service) => (
+                        <Service service={service}></Service>
+                    ))}
+                </Row>
+            </Container>
         </div>
     );
 };
