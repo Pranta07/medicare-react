29920bd15e29b23c2b87942a8c38b1ac5f686aeb
Pranta07 <palpranta@gmail.com>
2021-10-19 03:05:27 +0600

    about-page done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import aboutImg from "../../images/Doctors-pana.svg";
 
 const About = () => {
     return (
-        <div>
-            <h1>This is about us page.</h1>
-        </div>
+        <Container>
+            <Row className="align-items-center">
+                <Col>
+                    <h1 className="fw-bold text-dark">About Us</h1>
+                    <p className="text-secondary">
+                        Medicure provides progressive, and affordable
+                        healthcare, accessible on mobile and online for
+                        everyone.
+                    </p>
+                    <Link to="/home">
+                        <Button>Back to Home</Button>
+                    </Link>
+                </Col>
+                <Col>
+                    <div>
+                        <img src={aboutImg} alt="" />
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
