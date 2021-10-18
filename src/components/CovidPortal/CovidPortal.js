22c0f42f4a14254ef80b582c249d7bb7bb555418
Pranta07 <palpranta@gmail.com>
2021-10-19 02:51:37 +0600

    covid-portal done

--- src/components/CovidPortal/CovidPortal.js
+++ src/components/CovidPortal/CovidPortal.js
@@ -1,9 +1,102 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import portalImg from "../../images/undraw_medicine.svg";
+import bdFlag from "../../images/bangladesh-flag.jpg";
+import "./CovidPortal.css";
 
 const CovidPortal = () => {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch("https://covid-api.mmediagroup.fr/v1/cases?country=Bangladesh")
+            .then((res) => res.json())
+            .then((data) => setData(data.All));
+    }, []);
+
     return (
         <div>
-            <h1>This is Covid Portal</h1>
+            <h1 className="text-center fw-bold">Covid Portal</h1>
+            <hr className="mx-auto w-25" />
+            <p className="text-center text-secondary">
+                <small>Be Aware, Stay Healthy!</small>
+            </p>
+            <div className="container pb-3">
+                <div className="row align-items-center">
+                    <div className="col-lg-5">
+                        <div>
+                            <img src={portalImg} alt="" className="w-100" />
+                        </div>
+                        <div>
+                            <h1>Be Aware, Stay Healthy!</h1>
+                            <p className="text-secondary">
+                                <small>
+                                    Welcome to COVID-19 information portal.
+                                </small>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-lg-7">
+                        <div className="row gx-3 gy-3">
+                            <div className="col-6">
+                                <div
+                                    className="p-3 rounded item"
+                                    style={{
+                                        backgroundColor: "lightpink",
+                                    }}
+                                >
+                                    <div style={{ color: "maroon" }}>
+                                        <h3>Confirmed</h3>
+                                        <p id="confirmed">{data?.confirmed}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-6">
+                                <div
+                                    className="p-3 rounded item"
+                                    style={{ backgroundColor: "lightblue" }}
+                                >
+                                    <div style={{ color: "blue" }}>
+                                        <h3>Active</h3>
+                                        <p>151,846</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-6">
+                                <div
+                                    className="p-3 rounded item"
+                                    style={{
+                                        backgroundColor: "lightgreen",
+                                    }}
+                                >
+                                    <div style={{ color: "green" }}>
+                                        <h3>Recovered</h3>
+                                        <p>{data?.confirmed - data?.deaths}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-6">
+                                <div
+                                    className="p-3 rounded item"
+                                    style={{
+                                        backgroundColor: "lightgray",
+                                    }}
+                                >
+                                    <h3>Deceased</h3>
+                                    <p>{data?.deaths}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <h5 className="pt-3 text-center">
+                            <img src={bdFlag} alt="" width="50" height="50" />
+                            <span className="text-success">
+                                {" "}
+                                {data.country}
+                            </span>{" "}
+                            | Updated:
+                            <span>{data?.updated}</span>
+                        </h5>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
