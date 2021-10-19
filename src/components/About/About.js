e7a92f57bc127657dd55f09792aea4392168ca3c
Pranta07 <palpranta@gmail.com>
2021-10-19 15:46:19 +0600

    about-page responsive

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,12 +4,17 @@ import React from "react";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import aboutImg from "../../images/Doctors-pana.svg";
+import "./About.css";
 
 const About = () => {
     return (
         <Container>
             <Row className="align-items-center">
-                <Col>
+                <Col
+                    xs={12}
+                    md={6}
+                    className="order-sm-0 order-1 p-2 about-desc"
+                >
                     <h1 className="fw-bold text-dark">About Us</h1>
                     <p className="text-secondary">
                         Medicure provides progressive, and affordable
@@ -23,7 +28,7 @@ const About = () => {
                         </Button>
                     </Link>
                 </Col>
-                <Col>
+                <Col xs={12} md={6} className="order-0 order-sm-1">
                     <div>
                         <img src={aboutImg} alt="" />
                     </div>
