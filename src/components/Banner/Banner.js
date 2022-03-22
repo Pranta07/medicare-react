937784b164464be6056130b3d51983691ce687a3
Pranta07 <palpranta@gmail.com>
2022-03-22 16:34:37 +0600

    banner item added

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,7 +3,9 @@ import { Carousel, Container } from "react-bootstrap";
 import slideImg1 from "../../images/Carousel/undraw_doctors.svg";
 import slideImg2 from "../../images/Carousel/undraw_monitoring.svg";
 import slideImg3 from "../../images/Carousel/undraw_Online_information.svg";
+import slideImg4 from "../../images/Carousel/undraw_community.svg";
 import Typewriter from "typewriter-effect";
+
 const Banner = () => {
     return (
         <Container className="my-3 rounded-3 bg-light">
@@ -103,6 +105,41 @@ const Banner = () => {
                                 className="d-block w-100"
                                 src={slideImg3}
                                 alt="First slide"
+                                height="400"
+                            />
+                        </div>
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div className="m-3 row align-items-center">
+                        <div className="col-12 col-md-6">
+                            <h1 className="fw-bold">
+                                <Typewriter
+                                    onInit={(typewriter) => {
+                                        typewriter
+
+                                            .typeString("Blood")
+
+                                            .pauseFor(500)
+                                            .deleteAll()
+                                            .typeString(
+                                                "Blood Donors Community"
+                                            )
+                                            .start();
+                                    }}
+                                />
+                            </h1>
+                            <p className="text-secondary">
+                                A central blood donors community is always ready
+                                to help you! No need to worry for your patients.
+                                We will manage blood donors for you.
+                            </p>
+                        </div>
+                        <div className="col-12 col-md-6">
+                            <img
+                                className="d-block w-100"
+                                src={slideImg4}
+                                alt="Fourth slide"
                                 height="400"
                             />
                         </div>
